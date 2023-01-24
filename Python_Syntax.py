debf6bdbb25672aa802e3b7af77ebb181b11300b
DenisKDO <den_kim@kbtu.kz>
2023-01-24 22:45:37 +0600

    Python Syntax (adding one more example and syntax errors)

--- Python_Syntax.py
+++ Python_Syntax.py
@@ -1,2 +1,12 @@
 if 5 > 2:
   print("Five is greater than two!")
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+#Errors in syntax
+#if 5 > 2:
+#print("Five is greater than two!")
+#if 5 > 2:
+# print("Five is greater than two!")
+#       print("Five is greater than two!")
