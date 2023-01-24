b107ee0e515d5072926c9a7a74011e2164649845
DenisKDO <den_kim@kbtu.kz>
2023-01-24 23:00:55 +0600

    Python Strings adding examples

--- Python_Strings.py
+++ Python_Strings.py
@@ -1,5 +1,17 @@
+#Multiline strings
 a = '''Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua.'''
+print(a)
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+#Strings
+print("Hello")
+print('Hello')
+#Assign string to variable
+a = "Hello"
 print(a)
